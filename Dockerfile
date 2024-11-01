e99cfb251a8b03350eabe5a77936749456601d92
Paul Rakutt <keksgauner@gmail.com>
2024-11-01 15:39:27 +0100

    feat: new files for login / logged

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ FROM php:8.0-apache
 # Change the DocumentRoot in the Apache configuration
 RUN sed -i 's|/var/www/html|/var/www/html/public|g' /etc/apache2/sites-available/000-default.conf
 
-# Uncomment the following line to copy to the container
 COPY . /var/www/html/
---
+++ public/logged/index.php
@@ -0,0 +1,21 @@
+<?
+$parent_dir = dirname(__FILE__, 3);
+require_once realpath($parent_dir . "/vendor/autoload.php");
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Index</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="./../css/base.css" rel="stylesheet" />
+</head>
+
+<body class="relative isolate py-2 bg-gray-900">
+    <h1 class="text-2xl font-bold text-center animation-blink bg-red-500 text-white">
+        Index Site
+    </h1>
+</body>
+
+</html>
