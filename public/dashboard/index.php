2809ad9a35fb033e71d8e3b850eed11afdb39951
Paul Rakutt <keksgauner@gmail.com>
2024-11-01 16:53:08 +0100

    .

--- public/dashboard/index.php
+++ public/dashboard/index.php
@@ -1,6 +1,34 @@
 <?
+use \Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 $parent_dir = dirname(__FILE__, 3);
 require_once realpath($parent_dir . "/vendor/autoload.php");
+
+// Looing for .env at the root directory
+$dotenv = Dotenv\Dotenv::createImmutable($parent_dir);
+$dotenv->load();
+
+session_start();
+
+// Check if ywt is set
+if (!isset($_SESSION['ywt'])) {
+    header('Location: /login');
+    exit();
+}
+
+$jwt = $_SESSION['ywt'];
+$secretKey = $_ENV['JWT_SECRET_KEY'];
+
+try {
+    $decoded = JWT::decode($jwt, new Key($secretKey, 'HS256'));
+    // Token is valid, proceed to dashboard
+    print_r($decoded);
+} catch (Exception $e) {
+    // Token is invalid, redirect to login
+    header('Location: /logout');
+    exit();
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -16,6 +44,7 @@ require_once realpath($parent_dir . "/vendor/autoload.php");
     <h1 class="text-2xl font-bold text-center animation-blink bg-red-500 text-white">
         Dashboard Page
     </h1>
+    <a href="/logout" class="absolute top-0 right-0 bg-red-500 text-white p-2">Logout</a>
 </body>
 
 </html>
--- public/login/index.php
+++ public/login/index.php
@@ -1,6 +1,36 @@
 <?
+use \Firebase\JWT\JWT;
+
 $parent_dir = dirname(__FILE__, 3);
 require_once realpath($parent_dir . "/vendor/autoload.php");
+
+// Looing for .env at the root directory
+$dotenv = Dotenv\Dotenv::createImmutable($parent_dir);
+$dotenv->load();
+
+session_start();
+
+// Check if ywt is not set
+if (isset($_SESSION['ywt'])) {
+    header('Location: /dashboard');
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $password = $_POST['password'];
+
+    $secretKey = $_ENV['JWT_SECRET_KEY'];
+    $payload = [
+        'password' => $password,
+    ];
+
+    $jwt = JWT::encode($payload, $secretKey, 'HS256');
+
+    $_SESSION['jwt'] = $jwt;
+
+    header('Location: /dashboard');
+    exit();
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -16,6 +46,20 @@ require_once realpath($parent_dir . "/vendor/autoload.php");
     <h1 class="text-2xl font-bold text-center animation-blink bg-red-500 text-white">
         Login Page
     </h1>
+    <div class="grid place-content-center h-48">
+        <form action="#" method="post">
+            <div class="flex max-w-md gap-x-4">
+                <label for="password-id" class="sr-only">Password</label>
+                <input id="password-id" name="password" type="password" required
+                    class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    placeholder="Password" />
+                <button type="submit"
+                    class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                    Submit
+                </button>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
