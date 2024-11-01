ea0a541d7669c5c1f6cc36018d2e8ec1497186bf
Paul Rakutt <keksgauner@gmail.com>
2024-11-01 15:36:10 +0100

    fix: something

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 FROM php:8.0-apache
 
-COPY ./public /var/www/html/
+# Change the DocumentRoot in the Apache configuration
+RUN sed -i 's|/var/www/html|/var/www/html/public|g' /etc/apache2/sites-available/000-default.conf
+
+# Uncomment the following line to copy to the container
+COPY . /var/www/html/
--- public/index.php
+++ public/index.php
@@ -16,11 +16,6 @@ require_once realpath($parent_dir . "/vendor/autoload.php");
     <h1 class="text-2xl font-bold text-center animation-blink bg-red-500 text-white">
         Index Site
     </h1>
-    <div class="grid place-content-center h-48">
-        <a href="https://google.de"
-            class="text-sm rounded-md bg-indigo-500 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-            To Google</a>
-    </div>
 </body>
 
 </html>
