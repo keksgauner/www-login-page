7f321331875e83788071c15c89652852329dfd69
Paul Rakutt <keksgauner@gmail.com>
2024-11-01 15:51:27 +0100

    .

--- public/index.php
+++ public/index.php
@@ -16,6 +16,12 @@ require_once realpath($parent_dir . "/vendor/autoload.php");
     <h1 class="text-2xl font-bold text-center animation-blink bg-red-500 text-white">
         Index Site
     </h1>
+    <a href="./login" class="block text-center text-white bg-blue-500 py-2 mt-2">
+        Login
+    </a>
+    <a href="./logged" class="block text-center text-white bg-green-500 py-2 mt-2">
+        Logged
+    </a>
 </body>
 
 </html>
--- public/login/index.php
+++ public/login/index.php
@@ -14,7 +14,7 @@ require_once realpath($parent_dir . "/vendor/autoload.php");
 
 <body class="relative isolate py-2 bg-gray-900">
     <h1 class="text-2xl font-bold text-center animation-blink bg-red-500 text-white">
-        Index Site
+        Login Page
     </h1>
 </body>
 
